5fadcc16e4b96e5c16e9bd3e6b857327417e5578
fish155 <fishareverysmart@gmail.com>
2026-01-15 12:00:12 +0800

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -11,15 +11,26 @@ export default async function handler(req, res) {
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
-
-    const YOUTUBE_API_KEYS = process.env.YOUTUBE_API_KEYS.split(',');
-    const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
     
-    const { query, keyIndex } = req.query;
+    const YOUTUBE_API_KEYS = process.env.YOUTUBE_API_KEYS.split(',');
+    
+    const { query, keyIndex, maxResults = 5, type = 'search' } = req.query;
     const apiKey = YOUTUBE_API_KEYS[parseInt(keyIndex) % YOUTUBE_API_KEYS.length];
     
     try {
-        const response = await fetch(`${YOUTUBE_API_URL}${query}&key=${apiKey}`);
+        let youtubeUrl;
+        
+        if (type === 'search') {
+            // Search for videos
+            youtubeUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${encodeURIComponent(query)}&type=video&order=relevance&key=${apiKey}`;
+        } else if (type === 'statistics') {
+            // Get video statistics
+            youtubeUrl = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${encodeURIComponent(query)}&key=${apiKey}`;
+        } else {
+            return res.status(400).json({ error: 'Invalid type parameter' });
+        }
+        
+        const response = await fetch(youtubeUrl);
         const data = await response.json();
         
         return res.status(response.status).json({
